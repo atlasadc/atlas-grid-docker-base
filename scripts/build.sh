1ae4d43142247b5146fed982be72a048424f546b
Alessandro De Salvo <alessandro.desalvo@roma1.infn.it>
2026-01-16 19:54:15 +0100

    Update centos6 and slc6 with no vsyscall

--- scripts/build.sh
+++ scripts/build.sh
@@ -14,6 +14,11 @@ if [ -z "$OSREL" ] ; then
   echo "e.g. `basename $0` centos7"
   exit
 fi
+if [ "$OSREL" == "centos6" -o "$OSREL" == "slc6" ] ; then
+  PLATF="linux/amd64"
+else
+  PLATF="linux/amd64,linux/arm64"
+fi
 
-docker buildx build -t atlasadc/atlas-grid-${OSREL}-base --ulimit "nofile=1048576:1048576" --platform linux/amd64,linux/arm64 --push .
-docker buildx build -t registry.cern.ch/atlasadc/atlas-grid-${OSREL}-base --ulimit "nofile=1048576:1048576" --platform linux/amd64,linux/arm64 --push .
+docker buildx build -t atlasadc/atlas-grid-${OSREL}-base --ulimit "nofile=1048576:1048576" --platform ${PLATF} --push .
+docker buildx build -t registry.cern.ch/atlasadc/atlas-grid-${OSREL}-base --ulimit "nofile=1048576:1048576" --platform ${PLATF} --push .
